7f94cee0303ae60daae3ffda6d6af231fc4f3a40
Ehsan Shirvani <ehsanshirvani33@gmail.com>
2024-11-28 10:02:50 +0330

    created model admin for contact and use class meta
    created model admin for contact and use class meta

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from website.models import Contact
 
 # Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('name', 'subject','email', 'created_date')
+    search_fields = ['subject', 'messsage']
+
+admin.site.register(Contact, ContactAdmin)
