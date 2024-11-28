0347fe8344a5364c4a7a3cf37537039a40f59f97
Ehsan Shirvani <ehsanshirvani33@gmail.com>
2024-11-28 19:10:28 +0330

    use models for templates via url and parameter key
    use models for templates via url and parameter key and make single page dynamic a little

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from blog.models import Post
 
 def blog_view(request):
-    return render(request, 'blog/blog-home.html')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
 
-def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+def blog_single(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.counted_views += 1
+    post.save()
+    context = {'post': post}
+    return render(request, 'blog/blog-single.html', context)
