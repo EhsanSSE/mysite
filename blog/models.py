327abf97087b47080412a2ec888e085d01496b87
Ehsan Shirvani <ehsanshirvani33@gmail.com>
2024-11-29 20:14:57 +0330

    category model created and linked to post model
    category model created and linked to post model and put in template tags in blog home and blog detail pages

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=225)
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    # category
+    category = models.ManyToManyField(Category)
     # tag
     title =models.CharField(max_length=255)
     content = models.TextField()
