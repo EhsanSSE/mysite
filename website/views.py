87d6e370ca7a6cf473dfb7ff9b4c288289a422d4
Ehsan Shirvani <ehsanshirvani33@gmail.com>
2024-12-05 17:06:10 +0330

    forms file created and contact form created and tested
    forms file created and contact form created and tested in views and templates put this form and tested

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from website.forms import NameForm
 
 def index_view(request):
     return render(request, "website/index.html")
@@ -9,3 +10,18 @@ def about_view(request):
 
 def contact_view(request):
     return render(request, "website/contact.html")
+
+def test(requset):
+    if requset.method == 'POST':
+        form = NameForm(requset.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            print(name, email, subject, message)
+            return HttpResponse('done')
+        else:
+            return HttpResponse('not valid')
+    form = NameForm()
+    return render(requset, 'test2.html', {'form': form })
