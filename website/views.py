241fec04e4d1c9ed575e2182cb3894eb9bc9b6f1
Ehsan Shirvani <ehsanshirvani33@gmail.com>
2024-12-05 18:31:34 +0330

    newsletter form and model and view and url created
    ContactForm used in template and newsletter form and model and view and url created and use in templates

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from website.forms import NameForm
+from django.http import HttpResponse, HttpResponseRedirect
+from website.forms import NameForm, ContactForm, NewsletterForm
+
 
 def index_view(request):
     return render(request, "website/index.html")
@@ -9,19 +10,29 @@ def about_view(request):
     return render(request, "website/about.html")
 
 def contact_view(request):
-    return render(request, "website/contact.html")
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+    form = ContactForm()
+    return render(request, "website/contact.html", {'form': form})
+
+def newsletter_view(requset):
+    if requset.method == 'POST':
+        form = NewsletterForm(requset.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        return HttpResponseRedirect('/')
 
 def test(requset):
     if requset.method == 'POST':
-        form = NameForm(requset.POST)
+        form = ContactForm(requset.POST)
         if form.is_valid():
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            subject = form.cleaned_data['subject']
-            message = form.cleaned_data['message']
-            print(name, email, subject, message)
+            form.save()
             return HttpResponse('done')
         else:
             return HttpResponse('not valid')
-    form = NameForm()
+    form = ContactForm()
     return render(requset, 'test2.html', {'form': form })
