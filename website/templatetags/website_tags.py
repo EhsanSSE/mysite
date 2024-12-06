86aa02156109a4eb168fcb0643625908bfc6f922
Ehsan Shirvani <ehsanshirvani33@gmail.com>
2024-12-06 17:32:53 +0330

    latestpost in home page updated
    by templatetags added which posts was published and filtered it and in template linked categories which link with the post

--- website/templatetags/website_tags.py
+++ website/templatetags/website_tags.py
@@ -1,10 +1,13 @@
 from django import template
 from blog.models import Post
+from django.utils import timezone
 
 register = template.Library()
 
 @register.inclusion_tag('website/latest-blog-posts.html')
 def latest_blog_posts():
-    posts = Post.objects.filter(status=True).order_by('-published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now(), status=True).order_by('-published_date')
+    
+
     return {'posts': posts}
 
