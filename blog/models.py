2b64b17216bd5499aa626ee20e2b2dc3c644d7e9
Ehsan Shirvani <ehsanshirvani33@gmail.com>
2024-12-07 17:03:15 +0330

    sitemaps created for website app and blog app
    sitemaps created for website app and blog app and linked the url to show and make get absolute url for post models too

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -24,3 +25,7 @@ class Post(models.Model):
         ordering = ['-created_date']
     def __str__(self):
         return ('{}- {}'.format(self.id, self.title))
+    
+    def get_absolute_url(self):
+        return reverse("blog:single", kwargs={"pk": self.id})
+    
